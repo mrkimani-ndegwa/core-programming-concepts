61c5c53372f5e1f7ec26c5371d278cc8ed00acbb
Kimani Ndegwa <kimani@pave.solutions>
2020-05-06 21:49:51 +0300

    branchsums

---
+++ branchSum/branchSum.py
@@ -0,0 +1,18 @@
+def cl(node, total, l):
+    if node is None:
+        return
+    bSum = node.value + total
+    if node.left is None and node.right is None:
+        l.append(bSum)
+    cl(node.left, bSum, l)
+    cl(node.right, bSum, l)
+
+
+def branchSum(tree):
+    total = 0
+    l = []
+    cl(tree, total, l)
+    return l
+    
+    
+    
