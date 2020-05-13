ff5179c84e346813f1ee0f9e4cf1d20dd632c3aa
Kimani Ndegwa <kimani@pave.solutions>
2020-05-13 22:29:09 +0300

    Adds a few more solutions/options

--- fibonacci/fib.py
+++ fibonacci/fib.py
@@ -1,18 +1,39 @@
+import time
+
+print(time.time())
 def getNthFib(n):
     # iteratively
+    # space O (n)
+    # time O (n)
     sequence = [0,1]
     while len(sequence) <= n:
         num = sequence[len(sequence) - 1] + sequence[len(sequence) - 2]
         sequence.append(num)
 
-    return sequence
+    return sequence[n-1]
+
+def getNthFibOption(n):
+    sequence = [0,1]
+    counter = 3
+    while counter <= n:
+        nxt = sequence[0] + sequence[1]
+        sequence[0] = sequence[1]
+        sequence[1] = nxt
+        counter+=1
+    return sequence[1] if n > 1 else sequence[0]
 
 
 def getNthFibonnacci(n):
-    sequence = [0, 1]
-    if n not in sequence:
-        ge
+    # Poor time complexity galore
+    if n == 2:
+        return 1
+    elif n == 1:
+        return 0
+    else:
+        return getNthFibonnacci(n - 1) + getNthFibonnacci(n - 2)
 
 # allow the function to return a value.
 # recieive current
-print(getNthFib(35))
+print(getNthFib(35)) 
+print(getNthFibOption(35))
+print(getNthFibonnacci(35))
